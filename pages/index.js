b10e647383997382c0fc3d5ec8c6cf6e0b69fc40
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-02-04 23:07:05 +0530

    Data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-
-export default function Home() {
+import ArticleList from "../components/ArticleList";
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
@@ -8,6 +8,20 @@ export default function Home() {
         <meta name="keywords" content="web development, programming" />
       </Head>
       <h1>Welcome to Next JS</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
